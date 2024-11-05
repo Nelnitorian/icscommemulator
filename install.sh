8618f83cd4aab74714b7a54d7418904379d73be5
Juan José Rosendo <nelnitoreldestruior@gmail.com>
2024-11-05 19:19:39 +0100

    Update install.sh script to include -y flag for apt-get install commands

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,10 +15,12 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### TCPDUMP
-sudo apt-get install tcpdump
-sudo apt-get install python3 python3-pip
+sudo apt-get install -y tcpdump
+
+### PYTHON
+sudo apt-get install -y python3 python3-pip
 
 pip install -r requirements.txt
